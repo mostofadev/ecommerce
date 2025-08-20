261419534fdfbb309e129b8873bd6254ad94886c
Mostofakama <mostofakamalr2002@gmail.com>
2025-08-20 09:06:36 +0600

    shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,227 +1,156 @@
-import React from 'react'
-import Layout from './common/Layout'
-import Hero from './common/Hero'
-import ProductImg from '../assets/images/Mens/eight.jpg'
+import React from "react";
+import Layout from "./common/Layout";
+import ProductImg from "../assets/images/Mens/eleven.jpg";
+import ProductTowImg from "../assets/images/Mens/eight.jpg";
+import ProductThreeImg from "../assets/images/Mens/five.jpg";
+import ProductFourImg from "../assets/images/Mens/six.jpg";
+import ProductCard from "./common/Product/ProductCard";
+
 function Shop() {
+  const products = [
+    {
+      id: 1,
+      title: "Red Check Shirt For Men",
+      img: ProductImg,
+      price: 50,
+      oldPrice: 80,
+    },
+    {
+      id: 2,
+      title: "Blue Denim Jacket",
+      img: ProductTowImg,
+      price: 70,
+      oldPrice: 100,
+    },
+    {
+      id: 3,
+      title: "Classic White T-shirt",
+      img: ProductThreeImg,
+      price: 30,
+      oldPrice: 50,
+    },
+    {
+      id: 4,
+      title: "Black Formal Shirt",
+      img: ProductFourImg,
+      price: 60,
+      oldPrice: 90,
+    },
+    {
+      id: 5,
+      title: "Red Check Shirt For Men",
+      img: ProductImg,
+      price: 50,
+      oldPrice: 80,
+    },
+    {
+      id: 6,
+      title: "Blue Denim Jacket",
+      img: ProductTowImg,
+      price: 70,
+      oldPrice: 100,
+    },
+    {
+      id: 7,
+      title: "Classic White T-shirt",
+      img: ProductThreeImg,
+      price: 30,
+      oldPrice: 50,
+    },
+    {
+      id: 8,
+      title: "Black Formal Shirt",
+      img: ProductFourImg,
+      price: 60,
+      oldPrice: 90,
+    },
+  ];
+
   return (
-    <>
     <Layout>
-      <div>
-        <div className="container">
-        <nav aria-label="breadcrumb" className='py-4'>
+      <div className="container">
+        {/* Breadcrumb */}
+        <nav aria-label="breadcrumb" className="py-4">
           <ol className="breadcrumb">
-            <li className="breadcrumb-item"><a href="#">Home</a></li>
-            <li className="breadcrumb-item active" aria-current="page">Shop</li>
+            <li className="breadcrumb-item">
+              <a href="#">Home</a>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Shop
+            </li>
           </ol>
         </nav>
+
         <div className="row">
+          {/* Sidebar */}
           <div className="col-md-3">
             <div className="card shadow border-0 mb-3">
               <div className="card-body p-4">
-                <h3 className='mb-3'>Categories</h3>
+                <h3 className="mb-3">Categories</h3>
                 <ul>
-                  <li className='mb-2'>
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="" className='ps-2'>Kids</label>
+                  <li className="mb-2">
+                    <input type="checkbox" id="kids" />
+                    <label htmlFor="kids" className="ps-2">
+                      Kids
+                    </label>
                   </li>
-                  <li className='mb-2'>
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="" className='ps-2'>Mans</label>
+                  <li className="mb-2">
+                    <input type="checkbox" id="men" />
+                    <label htmlFor="men" className="ps-2">
+                      Men
+                    </label>
                   </li>
-                  <li className='mb-2'>
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="" className='ps-2'>Kids</label>
+                  <li className="mb-2">
+                    <input type="checkbox" id="women" />
+                    <label htmlFor="women" className="ps-2">
+                      Women
+                    </label>
                   </li>
                 </ul>
               </div>
-
             </div>
+
             <div className="card shadow border-0 mb-3">
               <div className="card-body p-4">
-                <h3 className='mb-3'>Brand</h3>
+                <h3 className="mb-3">Brand</h3>
                 <ul>
-                  <li className='mb-2'>
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="" className='ps-2'>Puma</label>
+                  <li className="mb-2">
+                    <input type="checkbox" id="puma" />
+                    <label htmlFor="puma" className="ps-2">
+                      Puma
+                    </label>
                   </li>
-                  <li className='mb-2'>
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="" className='ps-2'>Aron</label>
+                  <li className="mb-2">
+                    <input type="checkbox" id="aron" />
+                    <label htmlFor="aron" className="ps-2">
+                      Aron
+                    </label>
                   </li>
-                  <li className='mb-2'>
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="" className='ps-2'>Icons</label>
+                  <li className="mb-2">
+                    <input type="checkbox" id="icons" />
+                    <label htmlFor="icons" className="ps-2">
+                      Icons
+                    </label>
                   </li>
                 </ul>
               </div>
-
             </div>
           </div>
+
+          {/* Products */}
           <div className="col-md-9">
-          <div className="row">
-              <div className="col-md-3 col-6 mt-2">
-                          <div className="product card border-0">
-                            <div className="card-img">
-                              <img 
-                              src={ProductImg} 
-                              alt="" 
-                              className='w-100'
-                              />
-                            </div>
-                            <div className="card-body pt-3">
-                              <a href="">Red Check Shirt For Men</a>
-                              <div className="price">
-                                $50 <span className='text-decoration-line-through'>$80</span>
-              
-                              </div>
-                            </div>
-                              
-                          </div>
-              </div>
-              <div className="col-md-3 col-6 mt-2">
-                          <div className="product card border-0">
-                            <div className="card-img">
-                              <img 
-                              src={ProductImg} 
-                              alt="" 
-                              className='w-100'
-                              />
-                            </div>
-                            <div className="card-body pt-3">
-                              <a href="">Red Check Shirt For Men</a>
-                              <div className="price">
-                                $50 <span className='text-decoration-line-through'>$80</span>
-              
-                              </div>
-                            </div>
-                              
-                          </div>
-                </div>
-                <div className="col-md-3 col-6 mt-2">
-                            <div className="product card border-0">
-                              <div className="card-img">
-                                <img 
-                                src={ProductImg} 
-                                alt="" 
-                                className='w-100'
-                                />
-                              </div>
-                              <div className="card-body pt-3">
-                                <a href="">Red Check Shirt For Men</a>
-                                <div className="price">
-                                  $50 <span className='text-decoration-line-through'>$80</span>
+            <div className="row">
+              {products.map((product) => (
                 
-                                </div>
-                              </div>
-                                
-                            </div>
-                          </div>
-            <div className="col-md-3 col-6 mt-2">
-                        <div className="product card border-0">
-                          <div className="card-img">
-                            <img 
-                            src={ProductImg} 
-                            alt="" 
-                            className='w-100'
-                            />
-                          </div>
-                          <div className="card-body pt-3">
-                            <a href="">Red Check Shirt For Men</a>
-                            <div className="price">
-                              $50 <span className='text-decoration-line-through'>$80</span>
-            
-                            </div>
-                          </div>
-                            
-                        </div>
-                      </div>
-            <div className="col-md-3 col-6 mt-2">
-                        <div className="product card border-0">
-                          <div className="card-img">
-                            <img 
-                            src={ProductImg} 
-                            alt="" 
-                            className='w-100'
-                            />
-                          </div>
-                          <div className="card-body pt-3">
-                            <a href="">Red Check Shirt For Men</a>
-                            <div className="price">
-                              $50 <span className='text-decoration-line-through'>$80</span>
-            
-                            </div>
-                          </div>
-                            
-                        </div>
-                      </div>
-            
-                      <div className="col-md-3 col-6 mt-2">
-                        <div className="product card border-0">
-                          <div className="card-img">
-                            <img 
-                            src={ProductImg} 
-                            alt="" 
-                            className='w-100'
-                            />
-                          </div>
-                          <div className="card-body pt-3">
-                            <a href="">Red Check Shirt For Men</a>
-                            <div className="price">
-                              $50 <span className='text-decoration-line-through'>$80</span>
-            
-                            </div>
-                          </div>
-                            
-                        </div>
-              </div>
-              <div className="col-md-3 col-6 mt-2">
-                        <div className="product card border-0">
-                          <div className="card-img">
-                            <img 
-                            src={ProductImg} 
-                            alt="" 
-                            className='w-100'
-                            />
-                          </div>
-                          <div className="card-body pt-3">
-                            <a href="">Red Check Shirt For Men</a>
-                            <div className="price">
-                              $50 <span className='text-decoration-line-through'>$80</span>
-            
-                            </div>
-                          </div>
-                            
-                        </div>
-              </div>
-              <div className="col-md-3 col-6 mt-2">
-                        <div className="product card border-0">
-                          <div className="card-img">
-                            <img 
-                            src={ProductImg} 
-                            alt="" 
-                            className='w-100'
-                            />
-                          </div>
-                          <div className="card-body pt-3">
-                            <a href="">Red Check Shirt For Men</a>
-                            <div className="price">
-                              $50 <span className='text-decoration-line-through'>$80</span>
-            
-                            </div>
-                          </div>
-                            
-                        </div>
-              </div>
+                  <ProductCard {...product} />
+                
+              ))}
             </div>
           </div>
-        </div>
-
-     
         </div>
       </div>
     </Layout>
-    </>
-  )
+  );
 }
 
-export default Shop
+export default Shop;
